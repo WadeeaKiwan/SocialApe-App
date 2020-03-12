8209d81f9a65d52a35e8b2647979e649877cd099
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2020-03-12 01:21:09 +0100

    User Registration

--- socialape-functions/functions/src/index.ts
+++ socialape-functions/functions/src/index.ts
@@ -1,53 +1,103 @@
 import * as functions from "firebase-functions";
 const admin = require("firebase-admin");
-const express = require("express");
+const firebase = require("firebase");
+import firebaseConfig from "../config/firebaseConfig";
+
+const app = require("express")();
 
 admin.initializeApp();
 const db = admin.firestore();
 
-const app = express();
+firebase.initializeApp(firebaseConfig);
 
 // Get All Screams
-app.get("/screams", (req: any, res: any) => {
-  db.collection("screams")
-    .orderBy("createdAt", "desc")
-    .get()
-    .then((data: any[]) => {
-      let screams: any[] = [];
-      data.forEach((doc: any) => {
-        screams.push({
-          screamId: doc.id,
-          /**
-           * body: doc.data().body,
-           * userHandle: doc.data().userHandle,
-           * createdAt: doc.data().createdAt
-           * OR:
-           */
-          ...doc.data()
-        });
+app.get("/screams", async (req: any, res: any) => {
+  try {
+    let screams: any[] = [];
+    const data = await db
+      .collection("screams")
+      .orderBy("createdAt", "desc")
+      .get();
+
+    data.forEach((doc: any) => {
+      screams.push({
+        screamId: doc.id,
+        /**
+         * body: doc.data().body,
+         * userHandle: doc.data().userHandle,
+         * createdAt: doc.data().createdAt
+         * OR:
+         */
+        ...doc.data()
       });
-      return res.json(screams);
-    })
-    .catch((err: any) => console.error(err));
+    });
+    return res.status(200).json(screams);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: "Something went wrong" });
+  }
 });
 
 // Create a new scream
-app.post("/scream", (req: any, res: any) => {
-  const newScream = {
-    body: req.body.body,
-    userHandle: req.body.userHandle,
-    createdAt: new Date().toISOString()
+app.post("/scream", async (req: any, res: any) => {
+  try {
+    const newScream = {
+      body: req.body.body,
+      userHandle: req.body.userHandle,
+      createdAt: new Date().toISOString()
+    };
+
+    const doc = await db.collection("screams").add(newScream);
+    return res.json({ message: `document ${doc.id} created successfully` });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+// Signup route
+app.post("/signup", async (req: any, res: any) => {
+  let token: string, userId: string;
+
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle
   };
 
-  db.collection("screams")
-    .add(newScream)
-    .then((doc: any) => {
-      return res.json({ message: `document ${doc.id} created successfully` });
-    })
-    .catch((err: any) => {
-      res.status(500).json({ error: "Something went wrong" });
-      console.error(err);
-    });
+  try {
+    // TODO: validate data
+    const doc = await db.doc(`/users/${newUser.handle}`).get();
+
+    if (doc.exists) {
+      return res.status(400).json({ handle: "This handle is already taken" });
+    }
+    const data = await firebase
+      .auth()
+      .createUserWithEmailAndPassword(newUser.email, newUser.password);
+    userId = data.user.uid;
+
+    const idToken = await data.user.getIdToken();
+    token = idToken;
+
+    const userCredentials = {
+      handle: newUser.handle,
+      email: newUser.email,
+      createdAt: new Date().toISOString(),
+      userId
+    };
+
+    await db.doc(`/users/${newUser.handle}`).set(userCredentials);
+
+    return res.status(201).json({ token });
+  } catch (err) {
+    console.error(err);
+    if (err.code === "auth/email-already-in-use") {
+      return res.status(400).json({ email: "Email is already in use" });
+    }
+    return res.status(500).json({ error: err.code });
+  }
 });
 
 // Serve all routes with '/api' 'https://baseUrl.com/api'
