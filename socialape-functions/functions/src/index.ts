f086074fec6c6638ab79cf2d6b314426ff0bfaa7
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2020-03-15 14:19:34 +0100

    Validation & Login Route

--- socialape-functions/functions/src/index.ts
+++ socialape-functions/functions/src/index.ts
@@ -13,7 +13,7 @@ firebase.initializeApp(firebaseConfig);
 // Get All Screams
 app.get("/screams", async (req: any, res: any) => {
   try {
-    let screams: any[] = [];
+    const screams: any[] = [];
     const data = await db
       .collection("screams")
       .orderBy("createdAt", "desc")
@@ -55,19 +55,52 @@ app.post("/scream", async (req: any, res: any) => {
   }
 });
 
+const isEmpty = (string: string): boolean => {
+  if (string.trim() === "") return true;
+  else return false;
+};
+
+const isEmail = (email: string): boolean => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(regEx)) return true;
+  else return false;
+};
+
 // Signup route
 app.post("/signup", async (req: any, res: any) => {
   let token: string, userId: string;
 
-  const newUser = {
+  const newUser: {
+    email: string;
+    password: string;
+    confirmPassword: string;
+    handle: string;
+  } = {
     email: req.body.email,
     password: req.body.password,
     confirmPassword: req.body.confirmPassword,
     handle: req.body.handle
   };
 
+  // Validate data
+  const errors: any = {};
+
+  if (isEmpty(newUser.email)) {
+    errors.email = "Must not be empty!";
+  } else if (!isEmail(newUser.email)) {
+    errors.email = "Must be a valid email address!";
+  }
+
+  if (isEmpty(newUser.password)) errors.password = "Must not be empty!";
+  if (newUser.password !== newUser.confirmPassword)
+    errors.confirmPassword = "Passwords must match!";
+  if (isEmpty(newUser.handle)) errors.handle = "Must not be empty!";
+
+  if (Object.keys(errors).length > 0) {
+    return res.status(400).json(errors);
+  }
+
   try {
-    // TODO: validate data
     const doc = await db.doc(`/users/${newUser.handle}`).get();
 
     if (doc.exists) {
@@ -95,6 +128,42 @@ app.post("/signup", async (req: any, res: any) => {
     console.error(err);
     if (err.code === "auth/email-already-in-use") {
       return res.status(400).json({ email: "Email is already in use" });
+    }
+    return res.status(500).json({ error: err.code });
+  }
+});
+
+// Login route
+app.post("/login", async (req: any, res: any) => {
+  let token: string;
+
+  const user: {
+    email: string;
+    password: string;
+  } = {
+    email: req.body.email,
+    password: req.body.password
+  };
+
+  const errors: any = {};
+
+  if (isEmpty(user.email)) errors.email = "Must not be empty";
+  if (isEmpty(user.password)) errors.password = "Must not be empty";
+
+  if (Object.keys(errors).length > 0) {
+    return res.status(400).json(errors);
+  }
+
+  try {
+    const data = await firebase.auth().signInWithEmailAndPassword(user.email, user.password);
+    const idToken = await data.user.getIdToken();
+    token = idToken;
+
+    return res.status(201).json({ token });
+  } catch (err) {
+    console.error(err);
+    if (err.code === "auth/wrong-password") {
+      return res.status(403).json({ general: "Wrong credentials, please try again" });
     }
     return res.status(500).json({ error: err.code });
   }
