7b8501981e1a8bb176d9017296604dba69809c37
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2020-03-25 23:31:56 +0100

    Getting Started With React

---
+++ socialape-client/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+
+// Pages
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+// Components
+import Navbar from "./components/Navbar";
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Router>
+          <Navbar />
+          <div className='container'>
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/login' component={Login} />
+              <Route exact path='/signup' component={Signup} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default App;
